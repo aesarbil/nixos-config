2bc8737866155d4349e5a55595e546654377e343
aesarbil <aesarbil@gmail.com>
2026-05-14 04:37:03 +0200

    feat: syncthing declarado completamente en servicios.nix

--- modules/servicios.nix
+++ modules/servicios.nix
@@ -1,17 +1,15 @@
 # =============================================================================
 # Servicios del sistema
 # =============================================================================
-
 { config, pkgs, ... }:
-
 {
   # --- Tailscale ---
   services.tailscale = {
-    enable         = true;
-    extraUpFlags    = [ "--accept-routes" "--accept-dns=false" ];
+    enable       = true;
+    extraUpFlags = [ "--accept-routes" "--accept-dns=false" ];
   };
+  networking.nameservers = [ "192.168.1.1" "1.1.1.1" ]; # IP Flint2 en LAN
 
-  networking.nameservers = [ "192.168.1.1" "1.1.1.1" ];  # IP de tu Flint2 en LAN
   # --- Syncthing ---
   services.syncthing = {
     enable           = true;
@@ -19,6 +17,44 @@
     dataDir          = "/home/aesarbil";
     configDir        = "/home/aesarbil/.config/syncthing";
     openDefaultPorts = true;
+    overrideDevices  = true; # Nix manda, los cambios desde la web se ignoran
+    overrideFolders  = true;
+    systemService    = true;  # <-- añade esto
+
+    settings = {
+      devices = {
+        nas = {
+          id   = "5UQZP4R-FH2QARX-YILODD4-6KD6THT-VTM4WH7-P2DECOI-XNSQSEF-BHL5DA3";
+          name = "aesarbilNas";
+        };
+      };
+
+      folders = {
+        # Fotos — bidireccional
+        "tqkch-ej5uf" = {
+          label   = "Fotos";
+          path    = "/home/aesarbil/Imágenes/Fotos";
+          devices = [ "nas" ];
+          type    = "sendreceive";
+        };
+
+        # Familia — bidireccional
+        "Familia" = {
+          label   = "Familia";
+          path    = "/home/aesarbil/Familia";
+          devices = [ "nas" ];
+          type    = "sendreceive";
+        };
+
+        # Dotfiles — solo envía al NAS, el NAS no escribe aquí
+        "xpsze-fkc7b" = {
+          label   = "Dotfiles";
+          path    = "/home/aesarbil/.config";
+          devices = [ "nas" ];
+          type    = "sendonly";
+        };
+      };
+    };
   };
 
   # --- Impresión ---
@@ -29,8 +65,7 @@
 
   # --- xserver (necesario para xkb aunque no uses X11) ---
   services.xserver.enable = true;
-  
-  # --- Wifi
-  programs.nm-applet.enable = true;
 
+  # --- Wifi ---
+  programs.nm-applet.enable = true;
 }
