954ba3dad62c286714df576c4f3747c51f3fac10
aesarbil <aesarbil@gmail.com>
2026-05-19 03:12:10 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,7 @@
 # =============================================================================
 # NixOS - ThinkPad X260 (aesarbil)
 # =============================================================================
-
 { config, pkgs, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
@@ -18,24 +16,26 @@
     ./modules/usuario.nix
     ./modules/paquetes.nix
   ];
-
   # ===========================================================================
   # NIX
   # ===========================================================================
-
   nixpkgs.config.allowUnfree = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://cache.nixos.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
   nix.gc = {
     automatic = true;
     dates     = "weekly";
     options   = "--delete-older-than 30d";
   };
-
   # ===========================================================================
   # VERSIÓN (no cambiar manualmente)
   # ===========================================================================
-
   system.stateVersion = "25.11";
 }
